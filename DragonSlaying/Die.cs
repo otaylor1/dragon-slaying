8787a5417dfb8a7b677ac5a886c630cbfae71c93
Olivia Taylor <obt1@pitt.edu>
2016-06-23 10:31:31 -0400

    die roll

--- DragonSlaying/Die.cs
+++ DragonSlaying/Die.cs
@@ -9,15 +9,17 @@ namespace DragonSlaying
     public class Die
     {
         Random random;
-        public int NumberOfSides {get; set;}
+        public int NumberOfSides { get; set; }
 
         /// <summary>
         /// Constructs a 6-sided Die.
         /// </summary>
         public Die() // an alt. to what is below. Put " : this(6) " on this line and it will do the same thing.
         {
+
             random = new Random();
             NumberOfSides = 6;
+
         }
 
         /// <summary>
@@ -26,7 +28,7 @@ namespace DragonSlaying
         /// <param name="numberOfSides">The number of sides on the Die</param>
         public Die(int numberOfSides)
         {
-            // TODO
+
             NumberOfSides = numberOfSides;
             random = new Random();
 
@@ -38,8 +40,9 @@ namespace DragonSlaying
         /// <returns>A random number between 1 and <see cref="NumberOfSides"/></returns>
         public int Roll()
         {
-            // TODO
-            return 1;
+
+            return random.Next(1, NumberOfSides + 1);
+
         }
     }
 }
